207ae54957d4c14dfe71785844f43beed88e7a03
Rafael Figueiredo Martins Leite <135669849+RafaelFMLeite@users.noreply.github.com>
2024-09-30 14:28:48 -0300

    Delete operacoes/interacoes.py

--- operacoes/interacoes.py
+++
@@ -1,8 +0,0 @@
-def adicionar_convidado_evento(evento, convidado):
-    evento.adicionar_convidado(convidado)
-
-def adicionar_comida_evento(evento, comida):
-    evento.adicionar_comida(comida)
-
-def adicionar_bebida_evento(evento, bebida):
-    evento.adicionar_bebida(bebida)
